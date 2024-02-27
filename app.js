104e2c61ac70cec46bc1c9c0737944d0f91ce0a1
Mrnandan90 <nandan664464@gmail.com>
2024-02-27 23:15:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ sequelize
     // .sync({force: true})
     .sync()
     .then((result) => {
-        app.listen(3306);
+        app.listen(3000);
     })
     .catch((err) => {
         console.log(err);
