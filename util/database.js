530b751a22c59a92a4cbe89a49bf42e17a064e31
Mrnandan90 <nandan664464@gmail.com>
2024-02-27 23:38:29 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
         dialect: 'mysql',
         host: 'monorail.proxy.rlwy.net:18067',
         dialectOptions: {
-            useUTC: false, // Ensure UTC is not forced
+            // useUTC: false, // Ensure UTC is not forced
             timezone: '+05:30' // Set IST time zone explicitly
         }
     }
